e92c9bc62ea520421c77f8a191464dfa1068ab0f
Crowdbotics <engineering@crowdbotics.com>
2023-07-27 00:58:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ancient-poetry-dev-104712",
+  url: "https://ancient-poetry-dev-104712.botics.co",
+  api: "https://ancient-poetry-dev-104712.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
